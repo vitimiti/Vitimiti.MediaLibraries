1f431adc1b3fe67469311b6b937016d113e1685d
Victor Matia <vmatir@gmail.com>
2023-10-02 05:22:28 +0100

    Added the GLFW mouse buttons.
    Added an enumeration to represent the GLFW mouse buttons.
    
    Signed-off-by: Victor Matia <vmatir@gmail.com>

---
+++ Source/Windowing/Glfw.Net/MouseButton.cs
@@ -0,0 +1,16 @@
+namespace Vitimiti.MediaLibraries.Glfw.Net;
+
+public enum MouseButton
+{
+    Button1 = 0,
+    Button2,
+    Button3,
+    Button4,
+    Button5,
+    Button6,
+    Button7,
+    Button8,
+    Left = Button1,
+    Right = Button2,
+    Middle = Button3
+}
