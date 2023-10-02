6a757297b4dcfc7e4be8016c52a76b4c6bbade92
Victor Matia <vmatir@gmail.com>
2023-10-02 06:23:23 +0100

    Added the GLFW context client api attributes and hints.
    Added enumerations to represent the GLFW context client API attributes
    and hints.
    
    Signed-off-by: Victor Matia <vmatir@gmail.com>

---
+++ Source/Windowing/Glfw.Net/ContextClientApiAttribute.cs
@@ -0,0 +1,16 @@
+namespace Vitimiti.MediaLibraries.Glfw.Net;
+
+public enum ContextClientApiAttribute
+{
+    ClientApi = 0x00022001,
+    VersionMajor = 0x00022002,
+    VersionMinor = 0x00022003,
+    Revision = 0x00022004,
+    Robustness = 0x00022005,
+    OpenGlForwardCompat = 0x00022006,
+    OpenGlDebugContext = 0x00022007,
+    OpenGlProfile = 0x00022008,
+    ReleaseBehavior = 0x00022009,
+    NoError = 0x0002200A,
+    CreationApi = 0x0002200B
+}
