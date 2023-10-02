770e39f449b50e57ecafd27a55c67dae382a54d9
Victor Matia <vmatir@gmail.com>
2023-10-02 05:55:33 +0100

    Added the GLFW errors.
    Added an enumeration to represent the GLFW errors.
    
    Signed-off-by: Victor Matia <vmatir@gmail.com>

---
+++ Source/Windowing/Glfw.Net/Error.cs
@@ -0,0 +1,16 @@
+namespace Vitimiti.MediaLibraries.Glfw.Net;
+
+public enum Error
+{
+    NoError = 0,
+    NotInitialized = 0x00010001,
+    NoCurrentContext = 0x00010002,
+    InvalidEnum = 0x00010003,
+    InvalidValue = 0x00010004,
+    OutOfMemory = 0x00010005,
+    ApiUnavailable = 0x00010006,
+    VersionUnavailable = 0x00010007,
+    PlatformError = 0x00010008,
+    FormatUnavailable = 0x00010009,
+    NoWindowContext = 0x0001000A
+}
