7d12df09ecb9e18f3ab85c4ea6f0807b6b66e310
Victor Matia <vmatir@gmail.com>
2023-10-02 05:52:23 +0100

    Added the GLFW gamepad buttons.
    Added an enumeration representing the GLFW gamepad buttons.
    
    Signed-off-by: Victor Matia <vmatir@gmail.com>

---
+++ Source/Windowing/Glfw.Net/GamepadButton.cs
@@ -0,0 +1,24 @@
+namespace Vitimiti.MediaLibraries.Glfw.Net;
+
+public enum GamepadButton
+{
+    A,
+    B,
+    X,
+    Y,
+    LeftBumper,
+    RightBumper,
+    Back,
+    Start,
+    Guide,
+    LeftThumb,
+    RightThumb,
+    DpadUp,
+    DpadRight,
+    DpadDown,
+    DpadLeft,
+    Cross = A,
+    Circle = B,
+    Square = X,
+    Triangle = Y
+}
