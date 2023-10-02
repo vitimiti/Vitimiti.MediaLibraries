75068ea6c3ed2cefe215d90c557f6a9c9aec08ec
Victor Matia <vmatir@gmail.com>
2023-10-02 06:00:13 +0100

    Added the GLFW window attributes and hints.
    Added enumerations that represent the GLFW window attributes and hints
    
    Signed-off-by: Victor Matia <vmatir@gmail.com>

---
+++ Source/Windowing/Glfw.Net/WindowHint.cs
@@ -0,0 +1,15 @@
+namespace Vitimiti.MediaLibraries.Glfw.Net;
+
+public enum WindowHint
+{
+    Focused = 0x00020001,
+    Resizable = 0x00020003,
+    Visible = 0x00020004,
+    Decorated = 0x00020005,
+    Iconify = 0x00020006,
+    Floating = 0x00020007,
+    Maximized = 0x00020008,
+    CenterCursor = 0x00020009,
+    TransparentFramebuffer = 0x0002000A,
+    FocusOnShow = 0x0002000C
+}
