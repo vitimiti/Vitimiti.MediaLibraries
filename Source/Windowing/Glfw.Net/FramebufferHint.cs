7f6149b5f295263079528e660a5f2696b48e392a
Victor Matia <vmatir@gmail.com>
2023-10-02 06:18:38 +0100

    Added the GLFW framebuffer hints.
    Added an enumeration to represent the GLFW framebuffer hints.
    
    Signed-off-by: Victor Matia <vmatir@gmail.com>

---
+++ Source/Windowing/Glfw.Net/FramebufferHint.cs
@@ -0,0 +1,21 @@
+namespace Vitimiti.MediaLibraries.Glfw.Net;
+
+public enum FramebufferHint
+{
+    RedBits = 0x00021001,
+    GreenBits = 0x00021002,
+    BlueBits = 0x00021003,
+    AlphaBits = 0x00021004,
+    DepthBits = 0x00021005,
+    StencilBits = 0x00021006,
+    AccumRedBits = 0x00021007,
+    AccumGreenBits = 0x00021008,
+    AccumBlueBits = 0x00021009,
+    AccumAlphaBits = 0x0002100A,
+    AuxBuffers = 0x0002100B,
+    Stereo = 0x0002100C,
+    Samples = 0x0002100D,
+    SrgbCapable = 0x0002100E,
+    RefreshRate = 0x0002100F,
+    DoubleBuffer
+}
