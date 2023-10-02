169c31de3e3640f49c016414c2aa1af56f5bb055
Victor Matia <vmatir@gmail.com>
2023-10-02 05:50:18 +0100

    Added the GLFW joysticks.
    Added the enumeration for the GLFW joysticks.
    
    Signed-off-by: Victor Matia <vmatir@gmail.com>

---
+++ Source/Windowing/Glfw.Net/Joystick.cs
@@ -0,0 +1,21 @@
+namespace Vitimiti.MediaLibraries.Glfw.Net;
+
+public enum Joystick
+{
+    Joystick1,
+    Joystick2,
+    Joystick3,
+    Joystick4,
+    Joystick5,
+    Joystick6,
+    Joystick7,
+    Joystick8,
+    Joystick9,
+    Joystick10,
+    Joystick11,
+    Joystick12,
+    Joystick13,
+    Joystick14,
+    Joystick15,
+    Joystick16
+}
