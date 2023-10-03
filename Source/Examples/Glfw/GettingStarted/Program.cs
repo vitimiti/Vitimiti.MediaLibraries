01f3b23110a02ed28e30d00d9f980644b1940a02
Victor Matia <vmatir@gmail.com>
2023-10-03 05:01:43 +0100

    Updated the GLFW example.
    Now the GLFW example uses the new system.
    
    Signed-off-by: Victor Matia <vmatir@gmail.com>

--- Source/Examples/Glfw/GettingStarted/Program.cs
+++ Source/Examples/Glfw/GettingStarted/Program.cs
@@ -1,40 +1,10 @@
-﻿using System.Drawing;
-using System.Runtime.InteropServices;
+﻿using Vitimiti.MediaLibraries.Glfw.Net;
 
-using Vitimiti.MediaLibraries.Glfw.Net;
-
-try
-{
-    Glfw.SetErrorCallback(ErrorCallback);
-    if (!Glfw.Init())
-    {
-        (Error code, string? description) = Glfw.GetError();
-        throw new ExternalException(description, (int)code);
-    }
-
-    Window.SetHint(Hint.ContextVersionMajor, 2);
-    Window.SetHint(Hint.ContextVersionMinor, 0);
-
-    using Window? window = Glfw.CreateWindow(new Size(640, 480), "Getting Started (GLFW)", null, null);
-    if (window is null)
-    {
-        (Error code, string? description) = Glfw.GetError();
-        throw new ExternalException(description, (int)code);
-    }
-
-    while (!window.ShouldClose)
-    {
-        // Add processing here
-    }
-}
-finally
-{
-    Glfw.Terminate();
-}
+_ = Error.SetCallback(ErrorCallback);
 
 return;
 
-void ErrorCallback(Error code, string? description)
+void ErrorCallback(ErrorCode code, string? description)
 {
     Console.WriteLine($"[{code}] {description}");
 }
